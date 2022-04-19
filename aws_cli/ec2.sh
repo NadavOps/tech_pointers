9f52e903bcfee3e1b98d7561387f07f378f12b70
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-04-19 11:14:44 +0300

    ec2

--- aws_cli/ec2.sh
+++ aws_cli/ec2.sh
@@ -1,3 +1,4 @@
+# baseline networking: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/general-purpose-instances.html#general-purpose-network-performance
 aws ec2 describe-instance-types \
     --filters "Name=instance-type,Values=c5.*" \
     --query "InstanceTypes[].[InstanceType, NetworkInfo.NetworkPerformance]" \
