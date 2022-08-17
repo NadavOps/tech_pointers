9e7c25ecccc5885e308db1ac34f7620554bfe381
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-08-17 13:30:36 +0300

    mysql

--- mysql/mysql_commands.sh
+++ mysql/mysql_commands.sh
@@ -1,33 +1,38 @@
 MYSQL_HOST=host
 MYSQL_HOST_PORT=3306
-MYSQL_USER=user
-MYSQL_PASS=pass
+MYSQL_USER=user_name
+MYSQL_PASS=some_made_up_password
 
-connect to host:
+# Connect to host:
 mysql -h $MYSQL_HOST -P $MYSQL_HOST_PORT -u $MYSQL_USER -p $MYSQL_PASS
 
-Search a user:
+# Search a user:
 SELECT User, Host FROM mysql.user;
 SELECT User, Host FROM mysql.user WHERE User Like 'user_name';
 
-Show prievilleges:
+# User basics:
+CREATE USER 'user_name'@'%' IDENTIFIED BY 'some_made_up_password';
+ALTER USER 'user_name'@'%' IDENTIFIED BY 'some_other_password';
+DROP USER 'user_name'@'%';
+
+# Show prievilleges:
 SHOW GRANTS FOR 'user_name';
 
-CREATE USER 'delete_me_user_asap'@'%' IDENTIFIED BY 'delete_me_user_asap1234%$!@';
-grant select on information_schema.* to 'delete_me_user_asap'@'%' identified by 'delete_me_user_asap1234%$!@';
-GRANT SELECT ON *.* TO 'delete_me_user_asap'@'%';
-GRANT SELECT, PROCESS, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT, SHOW VIEW ON *.* TO 'delete_me_user_asap'@'%';
+# Grant prievilleges:
+GRANT SELECT ON schema_name.* TO 'user_name'@'%'; # <-- Read Only
+GRANT SELECT ON schema_name.* TO 'user_name'@'%' IDENTIFIED BY 'some_made_up_password'; # <-- Read Only + password
+GRANT SELECT,INSERT,UPDATE,DELETE,EXECUTE,SHOW VIEW,CREATE, \
+    ALTER,REFERENCES,INDEX,CREATE VIEW,CREATE ROUTINE,ALTER ROUTINE, \
+    EVENT,DROP,TRIGGER,CREATE TEMPORARY TABLES,LOCK TABLES ON schema_name.* TO 'user_name'@'%'; # <-- Write User
+GRANT SELECT, PROCESS, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT, SHOW VIEW ON *.* TO 'user_name'@'%'; # <-- replication user
 
-DROP USER 'delete_me_user_asap'@'%';
-
-ALTER USER 'delete_me_user_asap'@'%' IDENTIFIED BY 'delete_me_user_asap1234_new_pass';
 FLUSH PRIVILEGES;
 
-Process list:
-SHOW FULL PROCESSLIST
+# Process list:
+SHOW FULL PROCESSLIST;
 SELECT * FROM information_schema.processlist WHERE `USER` LIKE 'event_scheduler';
 
-Global variables:
+# Global variables:
 SHOW GLOBAL VARIABLES LIKE 'event%';
 call mysql.rds_show_configuration;
 call mysql.rds_set_configuration('binlog retention hours', 24);
