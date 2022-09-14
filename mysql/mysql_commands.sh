0536b5f250b3030de701024471cd66d4e1d14411
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-09-14 17:14:39 +0300

    mysql

--- mysql/mysql_commands.sh
+++ mysql/mysql_commands.sh
@@ -4,7 +4,7 @@ MYSQL_USER=user_name
 MYSQL_PASS=some_made_up_password
 
 # Connect to host:
-mysql -h $MYSQL_HOST -P $MYSQL_HOST_PORT -u $MYSQL_USER -p $MYSQL_PASS
+mysql -h $MYSQL_HOST -P $MYSQL_HOST_PORT -u $MYSQL_USER -p$MYSQL_PASS
 
 # Search a user:
 SELECT User, Host FROM mysql.user;
@@ -36,3 +36,15 @@ SELECT * FROM information_schema.processlist WHERE `USER` LIKE 'event_scheduler'
 SHOW GLOBAL VARIABLES LIKE 'event%';
 call mysql.rds_show_configuration;
 call mysql.rds_set_configuration('binlog retention hours', 24);
+
+# Triggers:
+SHOW TRIGGERS in schema_name;
+DROP TRIGGER schema_name.trigger_name;
+
+# Interactive mysql commands from shell:
+mysql -h $MYSQL_HOST -P $MYSQL_HOST_PORT -u $MYSQL_USER -p$MYSQL_PASS < sql_script_file.sql
+mysql -h $MYSQL_HOST -P $MYSQL_HOST_PORT -u $MYSQL_USER -p$MYSQL_PASS < sql_script_file.sql > csv_file.csv
+
+# Assumption1 -> show locking processes?
+show variables like 'innodb_lock_wait_timeout';
+SELECT * FROM INFORMATION_SCHEMA.PROCESSLIST WHERE COMMAND = "Sleep" AND TIME > 60;
