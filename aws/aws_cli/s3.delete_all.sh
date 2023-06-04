c2f273837f5356de0589e874d339ad81df5097be
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-06-04 18:50:06 +0300

    delete s3 based on input keys

---
+++ aws/aws_cli/s3.delete_all.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+delete_bucket_or_set_lifecycle_rules_for_deletion () {
+    local bucket_name
+    bucket_name="$1"
+    bucket_response=$(aws s3api list-objects-v2 --bucket "$bucket_name" --max-items 1) || exit 1
+    if [[ "$bucket_response" == "" ]]; then
+        echo "Empty bucket, deleting"
+        aws s3api delete-bucket --bucket "$bucket_name"
+    else
+        echo "NOT empty"
+        # Create lifecyclerules according to https://repost.aws/knowledge-center/s3-empty-bucket-lifecycle-rule
+        echo "Checking current lifecycle configuration"
+        aws s3api get-bucket-lifecycle-configuration --bucket "$bucket_name" --no-cli-pager
+        echo "Updating lifecycle configuration"
+        aws s3api put-bucket-lifecycle-configuration \
+        --bucket $bucket_name \
+        --lifecycle-configuration '{
+            "Rules": [
+                {
+                    "ID": "ExpireObjects",
+                    "Filter": {},
+                    "Status": "Enabled",
+                    "Expiration": {
+                        "Days": 1
+                    },
+                    "NoncurrentVersionExpiration": {
+                        "NoncurrentDays": 1
+                    },
+                    "AbortIncompleteMultipartUpload": {
+                        "DaysAfterInitiation": 1
+                    }
+                },
+                {
+                    "ID": "DeleteMarkers",
+                    "Filter": {},
+                    "Status": "Enabled",
+                    "Expiration": {
+                        "ExpiredObjectDeleteMarker": true
+                    }
+                }
+            ]
+        }'
+        # aws s3 rm "s3://$bucket_name" --recursive
+    fi
+}
+
+# Get the AWS account ID
+export AWS_ACCESS_KEY_ID="$1"
+export AWS_SECRET_ACCESS_KEY="$2"
+SPECIFIC_BUCKET="${3-}"
+
+if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+    echo "ERROR: provide param #1 AWS_ACCESS_KEY_ID ($AWS_ACCESS_KEY_ID), and param #2 AWS_SECRET_ACCESS_KEY ($AWS_SECRET_ACCESS_KEY)"
+    exit 1
+fi
+
+if [[ -z "$SPECIFIC_BUCKET" ]]; then
+    echo "ERROR: provide param #3. either a pecific bucket to delete, or the parameter DELETE_ALL_BUCKETS_IN_ACCOUNT"
+    exit 1
+fi
+
+echo "INFO: Running as:"
+aws sts get-caller-identity
+echo "INFO: Press enter to continue or ctrl+c to cancel"
+read
+
+if [[ "$SPECIFIC_BUCKET" == "DELETE_ALL_BUCKETS_IN_ACCOUNT" ]]; then
+    echo "INFO: Attempting deletion for all buckets in the account"
+    for bucket_name in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do
+        echo "Processing bucket $bucket_name"
+
+        delete_bucket_or_set_lifecycle_rules_for_deletion "$bucket_name"
+
+        echo "======== Next Bucket ========="
+    done
+else
+    echo "INFO: Attempting to delete bucket $SPECIFIC_BUCKET"
+    delete_bucket_or_set_lifecycle_rules_for_deletion "$SPECIFIC_BUCKET"
+fi
