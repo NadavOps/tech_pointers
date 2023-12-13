4925c8c666b7e65d2173e3e4cdd1de1b8271d265
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-12-13 12:27:54 +0200

    lambda_at_edge.get_names.sh

--- aws/aws_cli/lambda_at_edge.get_names.sh
+++ aws/aws_cli/lambda_at_edge.get_names.sh
@@ -8,18 +8,26 @@ for distribution_id in $(echo "${cloudfront_distributions}" | jq -r '.[]'); do
     echo "Distribution ID: ${distribution_id}"
 
     # Get Lambda@Edge associations for viewer-request and origin-request events
-    viewer_request_function=$(aws cloudfront get-distribution-config --id "${distribution_id}" --query 'DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[?EventType==`viewer-request`].LambdaFunctionARN' --output json)
-    origin_request_function=$(aws cloudfront get-distribution-config --id "${distribution_id}" --query 'DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[?EventType==`origin-request`].LambdaFunctionARN' --output json)
+    viewer_request_function_arn=$(aws cloudfront get-distribution-config --id "${distribution_id}" --query 'DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[?EventType==`viewer-request`].LambdaFunctionARN' --output json)
+    origin_request_function_arn=$(aws cloudfront get-distribution-config --id "${distribution_id}" --query 'DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[?EventType==`origin-request`].LambdaFunctionARN' --output json)
+    viewer_response_function_arn=$(aws cloudfront get-distribution-config --id "${distribution_id}" --query 'DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[?EventType==`viewer-response`].LambdaFunctionARN' --output json)
+    origin_response_function_arn=$(aws cloudfront get-distribution-config --id "${distribution_id}" --query 'DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[?EventType==`origin-response`].LambdaFunctionARN' --output json)
 
     # Print Lambda function details if associated
-    if [ "${viewer_request_function}" != "null" ]; then
-        echo "  Viewer Request Lambda Function: ${viewer_request_function}"
-        # aws lambda get-function --function-name "$(echo "${viewer_request_function}" | jq -r '.[0]' | cut -d':' -f7)"
+    if [ "${viewer_request_function_arn}" != "null" ]; then
+        echo "  Viewer Request Lambda Function: ${viewer_request_function_arn}"
     fi
 
-    if [ "${origin_request_function}" != "null" ]; then
-        echo "  Origin Request Lambda Function: ${origin_request_function}"
-        # aws lambda get-function --function-name "$(echo "${origin_request_function}" | jq -r '.[0]' | cut -d':' -f7)"
+    if [ "${origin_request_function_arn}" != "null" ]; then
+        echo "  Origin Request Lambda Function: ${origin_request_function_arn}"
+    fi
+
+    if [ "${viewer_response_function_arn}" != "null" ]; then
+        echo "  Viewer Response Lambda Function: ${viewer_response_function_arn}"
+    fi
+
+    if [ "${origin_response_function_arn}" != "null" ]; then
+        echo "  Origin Response Lambda Function: ${origin_response_function_arn}"
     fi
 
     echo "---"
