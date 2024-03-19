a2f8232e9eaf8e9c43144ea78d89f59776be7c49
NadavOps <72969918+NadavOps@users.noreply.github.com>
2024-03-19 12:42:54 +0200

    docker

--- docker/README.md
+++ docker/README.md
@@ -1,11 +1,18 @@
 # Docker
 
 ### Table of Content
+* [Commands](#commands)
 * [Multi arch containers](#multi-arch-containers)
 * [Links](#links)
 
-## Multi arch containers
+## Commands
+```bash
+docker history image_id --format "{{.CreatedBy}}" --no-trunc > history.csv
+docker history image_id --format "table{{.ID}}, {{.CreatedBy}}" --no-trunc > history.csv
 ```
+
+## Multi arch containers
+```bash
 # Using manifests
 container_image="example_image:latest"
 private_registry="input_here"
@@ -35,3 +42,4 @@ docker manifest inspect "$private_registry":"$manifest_tag"
   * [Article 1](https://www.howtogeek.com/devops/how-to-use-multiple-docker-build-contexts-to-streamline-image-assembly/)
   * [Article 1](https://www.docker.com/blog/dockerfiles-now-support-multiple-build-contexts/)
 * [Docker compose general guide](https://gabrieltanner.org/blog/docker-compose/)
+* [Local registry commands](https://stackoverflow.com/questions/31251356/how-to-get-a-list-of-images-on-docker-registry-v2)
--- docker/dockerhub.get_repo_tags.py
+++ docker/dockerhub.get_repo_tags.py
@@ -2,9 +2,10 @@ import argparse
 import requests
 import pprint
 
-def get_dockerhub_tags(repository):
-    api_url = f"https://hub.docker.com/v2/namespaces/library/repositories/{repository}/tags?page_size=100"
-    tags = []
+def get_dockerhub_tags(namespace, repository):
+    api_url = f"https://hub.docker.com/v2/namespaces/{namespace}/repositories/{repository}/tags?page_size=100"
+    # tags_old_way = []
+    tags = {}
 
     while True:
         response = requests.get(api_url)
@@ -12,8 +13,19 @@ def get_dockerhub_tags(repository):
             data = response.json()
             data_results_field = data.get('results', [])
 
+            for image in data_results_field:
+                # print(image)
+                tag = image.get('name', 'didnt_find_tag')
+                digest = image.get('digest', 'didnt_find_digest')
+                tags[tag] = { "manifest_digest": digest }
+                for sub_image in image['images']:
+                    architecture = sub_image.get('architecture', 'didnt_find_architecture')
+                    sub_image_digest = sub_image.get('digest', 'didnt_find_architecture')
+                    tags[tag][architecture] = sub_image_digest
+                # print("======")
+
             # Extract tags from the current page
-            tags.extend(item['name'] for item in data_results_field)
+            # tags_old_way.extend(item['name'] for item in data_results_field)
 
             # Check if there is a next page
             next_page = data.get('next')
@@ -25,31 +37,46 @@ def get_dockerhub_tags(repository):
             print(f"Error: Unable to fetch tags. Status code: {response.status_code}")
             return None
 
-    pprint.pprint(tags)  # Print all tags
     return tags
 
 def filter_tags_by_prefix(tags, prefix):
-    filtered_tags = [tag for tag in tags if tag.startswith(prefix)]
+    filtered_tags = {}
+    for tag in tags:
+        if tag.startswith(prefix):
+            filtered_tags[tag] = tags[tag]
     return filtered_tags
 
 def main():
     parser = argparse.ArgumentParser(description='Retrieve and filter Docker Hub tags by prefix')
     parser.add_argument('-r', '--repository', type=str, required=True, help='Docker Hub repository')
-    parser.add_argument('-p', '--prefix', type=str, required=True, help='Prefix for filtering tags')
+    parser.add_argument('-n', '--namespace', type=str, default="library", help='The namespace to look at. in nadavops/tooling the namespace is nadavops')
+    parser.add_argument('-t', '--tag_prefix', type=str, default=None, help='Prefix for filtering tags')
+    parser.add_argument('-i', '--image_digest', type=str, default=None, help='I think this is to search for specific digest')
+    parser.add_argument('-s', '--show_more_info', action='store_true', help='output more')
 
     args = parser.parse_args()
 
-    repository = args.repository
-    prefix = args.prefix
+    tags = get_dockerhub_tags(args.namespace, args.repository)
 
-    tags = get_dockerhub_tags(repository)
+    if args.show_more_info:
+        pprint.pprint(tags)
 
     if tags is not None:
-        filtered_tags = filter_tags_by_prefix(tags, prefix)
+        if args.tag_prefix is not None:
+            tags = filter_tags_by_prefix(tags, args.tag_prefix)
+            print(f"\nTags filtered with the prefix '{args.tag_prefix}':")
+            for tag in reversed(tags):
+                if args.show_more_info:
+                    pprint.pprint({tag: tags[tag]})
+                else:
+                    print(tag)
 
-        print(f"\nTags with prefix '{prefix}':")
-        for tag in reversed(filtered_tags):
-            print(tag)
+        if args.image_digest is not None:
+            print(f"\nTags for image digest: '{args.image_digest}':")
+            for tag in tags:
+                for architecture in tags[tag]:
+                    if tags[tag][architecture] == args.image_digest:
+                        pprint.pprint({tag: {architecture: tags[tag][architecture]}})
     else:
         print("Exiting due to an error.")
 
